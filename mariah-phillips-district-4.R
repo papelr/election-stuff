bb5de4706c6b7ae2b6def90b3fa323355b807784
papelr <robertpapel@gmail.com>
2018-07-09 16:16:12 -0500

    loading in vote number data

---
+++ mariah-phillips-district-4.R
@@ -0,0 +1,28 @@
+#' ---------
+#' Title: TN District 4
+#' Subtitle: Can Mariah Phillips Win?
+#' Date: July 2018
+#' Author: Robert Papel
+#' ---------
+
+#'###### -------------**Libraries**---------------------- ######
+
+library(tidyverse)
+
+#'###### -------------**Loading Past Election Data**---------- ######
+
+# 2012 District 4 Data, number of votes per party
+Rep_2012 <- 128568
+Dem_2012 <- 102222
+Ind_2012 <- 0
+
+# 2014 District 4 Data, number of votes per party
+Rep_2014 <- 84815
+Dem_2014 <- 51357
+Ind_2014 <- 9246
+
+#2016 Distrct 4 Data, number of votes per party
+Rep_2016 <- 165796
+Dem_2016 <- 89141
+Ind_2016 <- 0
+
