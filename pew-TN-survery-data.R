b356a25d905f6e086afae805a0d14bf75066dc64
papelr <robertpapel@gmail.com>
2018-07-10 12:16:42 -0500

    looking at income, ideology, race w/ ggplot

--- pew-TN-survery-data.R
+++ pew-TN-survery-data.R
@@ -9,22 +9,36 @@
 
 library(tidyverse)
 library(foreign)
+library(stringr)
 
 pew_data <- read.spss("Typology 17 Public.sav", to.data.frame = TRUE)
 head(pew_data)
 
 #'###### -------------**Modeling**---------------------- ######
 
-pew_data %>% 
+# Race filter
+TN_race <- pew_data %>% 
   select(racecmb, age, income, party, ideo, state, marital) %>% 
   filter(state == "Tennessee") %>% 
-  group_by(state)
-  
+  group_by(state) %>% 
+  ggplot() +
+  geom_bar(aes(racecmb))
 
+# Ideology filter
+pew_data %>% 
+  select(racecmb, age, income, party, ideo, state, marital) %>%
+  filter(state == "Tennessee") %>% 
+  group_by(state) %>% 
+  ggplot() +
+  geom_bar(aes(ideo))
 
-# ideo:
-#   liberal
-#   moderate
-#   conservative
-#   very conservative
-#   very liberal
+# Income filter
+pew_data$income <- gsub('[[OR]]', '', pew_data$income) 
+pew_data %>% 
+  select(racecmb, age, income, party, ideo, state, marital) %>%
+  filter(state == "Tennessee",
+         income != "[VOL. DO NOT READ] Don't know/Refused",
+         income != "[VL. D NT EAD] Don't know/efused") %>%
+  group_by(income) %>% 
+  ggplot() +
+  geom_bar(aes(income))
