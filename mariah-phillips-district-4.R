a1edebe6a8c3edb8720783f6cc01f2e73817f086
papelr <robertpapel@gmail.com>
2018-07-09 20:17:57 -0500

    ggsave plots added

--- mariah-phillips-district-4.R
+++ mariah-phillips-district-4.R
@@ -15,25 +15,25 @@ library(tidyverse)
 # Combine 3 election years of data into a tibble, from:
 # http://history.house.gov/Institution/Election-Statistics/Election-Statistics/
 
-District_4 <- tibble("Year" = c(2012, 2014, 2016),
-                     "Republican Votes" = c(128568, 84815, 165796),
-                     "Democrat Votes" = c(102222, 51357, 89141),
-                     "Independent Votes" = c(0, 9246, 0))
+District_4 <- tibble("Year" = c(2010, 2012, 2014, 2016),
+                     "Republican Votes" = c(103969, 128568, 84815, 165796),
+                     "Democrat Votes" = c(70254, 102222, 51357, 89141),
+                     "Independent Votes" = c(7968, 0, 9246, 0))
 
-# Quick visualization
-party_colors <- c("E0162B", "0052A5")
-
-District_4 %>%
+# Visualization not standardized on a percent scale
+votes_numbers <- District_4 %>%
   mutate(Year = as.Date(paste0(Year, "/01/01"))) %>%
   gather(key, VoteCount, -Year) %>%
   ggplot(aes(Year, VoteCount, fill = key)) +
   geom_bar(stat = "identity") + 
+  scale_fill_manual(values = c("#0052A5", "gray", "#E0162B")) +
+  # coord_flip() +
   theme_bw() +
   labs(
     title = "TN District 4 Votes by Party",
-    subtitle = "Viability of Mariah Phillips",
+    subtitle = "Voting numbers, last 4 cycles",
     x = "Year",
-    y = "",
+    y = "Votes",
     caption = "Data from Clerk of the House, Plot by R. Papel") +
   theme(
     plot.title = element_text(face = "bold", size = 12),
@@ -45,5 +45,47 @@ District_4 %>%
     legend.background = element_rect(color = "grey70"), 
     axis.title.y = element_text(size = 11, face = "bold", color = "black"),
     axis.title.x = element_text(size = 11, face = "bold", color = "black")
-  ) 
+  ) +
+  guides(fill = guide_legend(title = "Legend")) +
+  scale_y_continuous(labels = scales::comma)
 
+# Graph with position = "fill"
+votes_standardized <- District_4 %>%
+  mutate(Year = as.Date(paste0(Year, "/01/01"))) %>%
+  gather(key, VoteCount, -Year) %>%
+  ggplot(aes(Year, VoteCount, fill = key)) +
+  geom_bar(stat = "identity", position = "fill") + 
+  scale_fill_manual(values = c("#0052A5", "gray", "#E0162B")) +
+  # coord_flip() +
+  theme_bw() +
+  labs(
+    title = "TN District 4 Votes by Party",
+    subtitle = "Voting numbers standardized, last 4 cycles",
+    x = "Year",
+    y = "Vote Proportion",
+    caption = "Data from Clerk of the House, Plot by R. Papel") +
+  theme(
+    plot.title = element_text(face = "bold", size = 12),
+    plot.caption = element_text(face = "italic"),
+    plot.subtitle = element_text(face = "italic"),
+    axis.ticks = element_line(colour = "grey70", size = 0.2),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"),
+    legend.background = element_rect(color = "grey70"), 
+    axis.title.y = element_text(size = 11, face = "bold", color = "black"),
+    axis.title.x = element_text(size = 11, face = "bold", color = "black")
+  ) +
+  guides(fill = guide_legend(title = "Legend")) 
+
+
+#'###### ---------------**ggsave**-------------------- ######
+
+ggsave(votes_numbers, file = "/Users/robertpapel/Documents/Personal_R_Stuff/election-stuff/Plots (ggsave)/votes_numbers_July9.png", 
+       device = "png",
+       width = 10,
+       height = 7)
+
+ggsave(votes_standardized, file = "/Users/robertpapel/Documents/Personal_R_Stuff/election-stuff/Plots (ggsave)/votes_standardized_July9.png", 
+       device = "png",
+       width = 10,
+       height = 7)
