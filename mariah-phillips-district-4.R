1305df9ba79522e3d1f0fbbd7a15ff4356e6932c
papelr <robertpapel@gmail.com>
2018-07-09 19:06:39 -0500

    stacked bar graph sorted out

--- mariah-phillips-district-4.R
+++ mariah-phillips-district-4.R
@@ -11,18 +11,39 @@ library(tidyverse)
 
 #'###### -------------**Loading Past Election Data**---------- ######
 
-# 2012 District 4 Data, number of votes per party
-Rep_2012 <- 128568
-Dem_2012 <- 102222
-Ind_2012 <- 0
+# Compile 3 election years of party voting in TN District 4:
+# Combine 3 election years of data into a tibble, from:
+# http://history.house.gov/Institution/Election-Statistics/Election-Statistics/
 
-# 2014 District 4 Data, number of votes per party
-Rep_2014 <- 84815
-Dem_2014 <- 51357
-Ind_2014 <- 9246
+District_4 <- tibble("Year" = c(2012, 2014, 2016),
+                     "Republican Votes" = c(128568, 84815, 165796),
+                     "Democrat Votes" = c(102222, 51357, 89141),
+                     "Independent Votes" = c(0, 9246, 0))
 
-#2016 Distrct 4 Data, number of votes per party
-Rep_2016 <- 165796
-Dem_2016 <- 89141
-Ind_2016 <- 0
+# Quick visualization
+party_colors <- c("E0162B", "0052A5")
+
+District_4 %>%
+  mutate(Year = as.Date(paste0(Year, "/01/01"))) %>%
+  gather(key, VoteCount, -Year) %>%
+  ggplot(aes(Year, VoteCount, fill = key)) +
+  geom_bar(stat = "identity") + 
+  theme_bw() +
+  labs(
+    title = "TN District 4 Votes by Party",
+    subtitle = "Viability of Mariah Phillips",
+    x = "Year",
+    y = "",
+    caption = "Data from Clerk of the House, Plot by R. Papel") +
+  theme(
+    plot.title = element_text(face = "bold", size = 12),
+    plot.caption = element_text(face = "italic"),
+    plot.subtitle = element_text(face = "italic"),
+    axis.ticks = element_line(colour = "grey70", size = 0.2),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"),
+    legend.background = element_rect(color = "grey70"), 
+    axis.title.y = element_text(size = 11, face = "bold", color = "black"),
+    axis.title.x = element_text(size = 11, face = "bold", color = "black")
+  ) 
 
